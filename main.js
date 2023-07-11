fee5ac89841380fd6d127622e03ef1fb36d0ab71
Bonello <geno.bonello@gmail.com>
2023-07-11 15:15:20 -0700

    final push

--- main.js
+++ main.js
@@ -15,69 +15,68 @@ const selectedCard = document.querySelectorAll(".selected-card")
 const cards = document.querySelectorAll(".card")
 const colors = ["red", "red", "blue", "blue", "green", "green", "yellow", "yellow", "orange", "orange", "purple", "purple"]
 
-shuffleArray(colors)
-
-function newGame(){
+function newGame() {
     shuffleArray(colors)
     winLoss.style.display = "flex"
     cardDiv.style.display = "grid"
-    cards.forEach(card => {card.style.backgroundColor = "rgb(173, 173, 173)"})
-}
+    cards.forEach(card => { cardsArr.push(card) })
+    cards.forEach(card => { card.style.backgroundColor = "rgb(173, 173, 173)" })
 
-cards.forEach(card => {cardsArr.push(card)})
-
-for (let i = 0; i < cardsArr.length; i++){
-    cardsArr[i].setAttribute("id", colors[i])
-}
-
-cards.forEach(card => {card.addEventListener("click", function(){
-    if (cardFlipped === false){
-    card.style.backgroundColor = card.id
-    currentCardColor = card.id
-    cardFlipped = true
-    card.setAttribute("class", "selected-card")
-    } else {
-        card.style.backgroundColor = card.id
-        if (card.id === currentCardColor){
-            cardFlipped = false
-            const selectedCard = document.querySelectorAll(".selected-card")
-            selectedCard.forEach(selectedCard => {selectedCard.removeAttribute("class", "selected-card")})
-            matches ++
-            checkWin()
-        } else {
-            tries --
-            triesLeft.innerHTML = `Tries: ${tries}`
-            card.setAttribute("class", "selected-card")
-            const selectedCard = document.querySelectorAll(".selected-card")
-            setTimeout(() => {
-                selectedCard.forEach(selectedCard => {selectedCard.style.backgroundColor = "rgb(173, 173, 173)"})
-            }, 1000)
-            cardFlipped = false
-            checkLoss()
-        }
+    for (let i = 0; i < cardsArr.length; i++) {
+        cardsArr[i].setAttribute("id", colors[i])
     }
-})})
+}
+
+cards.forEach(card => {
+    card.addEventListener("click", function () {
+        if (cardFlipped === false) {
+            card.style.backgroundColor = card.id
+            currentCardColor = card.id
+            cardFlipped = true
+            card.setAttribute("class", "selected-card")
+        } else {
+            card.style.backgroundColor = card.id
+            if (card.id === currentCardColor) {
+                cardFlipped = false
+                const selectedCard = document.querySelectorAll(".selected-card")
+                selectedCard.forEach(selectedCard => { selectedCard.removeAttribute("class", "selected-card") })
+                matches++
+                checkWin()
+            } else {
+                tries--
+                triesLeft.innerHTML = `Tries: ${tries}`
+                card.setAttribute("class", "selected-card")
+                const selectedCard = document.querySelectorAll(".selected-card")
+                setTimeout(() => {
+                    selectedCard.forEach(selectedCard => { selectedCard.style.backgroundColor = "rgb(173, 173, 173)" })
+                }, 1000)
+                cardFlipped = false
+                checkLoss()
+            }
+        }
+    })
+})
 
 function shuffleArray(colors) {
     for (let i = colors.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = colors[i];
-       colors[i] = colors[j];
+        colors[i] = colors[j];
         colors[j] = temp;
     }
 }
 
-function checkWin(){
-    if (matches === 6){
+function checkWin() {
+    if (matches === 6) {
         alert("Winner")
-        score ++
+        score++
         wins.innerHTML = `Wins: ${score}`
     }
 }
 
-function checkLoss(){
-    if (tries === 0){
-        loss ++
+function checkLoss() {
+    if (tries === 0) {
+        loss++
         alert("Loser!")
         losses.innerHTML = `Losses: ${loss}`
     }
